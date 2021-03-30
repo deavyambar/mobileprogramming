95083fcba47c6f62b709ac1b265ed2c911d289a7
Deavy Ambar Permatasari <sidedekecil@gmail.com>
2021-03-30 02:14:24 +0000

    Add new file

---
+++ Tugas-3/es6-3.js
@@ -0,0 +1,12 @@
+const newObject = {
+  firstName: "Harry",
+  lastName: "Potter Holt",
+  destination: "Hogwarts React Conf",
+  occupation: "Deve-wizard Avocado",
+  spell: "Vimulus Renderus!!!"
+}
+const {firstName, lastName, destination, occupation} = newObject;
+
+// Driver code
+console.log(firstName, lastName, destination, occupation)
+
