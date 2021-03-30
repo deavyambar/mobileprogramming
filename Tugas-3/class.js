a474a34ccb592e79e9fbc551484fd65c0cdeed91
Deavy Ambar Permatasari <sidedekecil@gmail.com>
2021-03-30 02:33:50 +0000

    add new class.js

--- Tugas-3/class.js
+++ Tugas-3/class.js
@@ -56,3 +56,46 @@ class Animal {
   console.log("buduk")
   kodok.jump() // "hop hop"
   console.log('')
+  
+//No.2-Function to class
+class Clock {
+
+    constructor({template})
+    {
+      this.template = template;
+    }
+  
+    render()
+    {
+      var date = new Date();
+  
+      var hours = date.getHours();
+      if (hours < 10) hours = '0' + hours;
+  
+      var mins = date.getMinutes();
+      if (mins < 10) mins = '0' + mins;
+  
+      var secs = date.getSeconds();
+      if (secs < 10) secs = '0' + secs;
+  
+      var output = this.template
+        .replace('h', hours)
+        .replace('m', mins)
+        .replace('s', secs);
+  
+      console.log(output);
+    }
+  
+    start()
+    {
+      this.timer = setInterval(this.render.bind(this), 1000)
+    }
+  
+    stop()
+    {
+      clearInterval(this.timer);
+    }
+  }
+  
+  var clock = new Clock({template: 'h:m:s'});
+  clock.start()
